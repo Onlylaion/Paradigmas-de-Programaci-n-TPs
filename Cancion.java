c456b0bcdeb350568ac2780f82167e0aba6c2a89
Onlylaion <marcosleonrodriguez2030@gmail.com>
2025-11-14 19:28:15 -0300

    Add Cancion class for concert song management

---
+++ Cancion.java
@@ -0,0 +1,136 @@
+package concierto;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class Cancion {
+	private String nombreCancion;
+	private double duracion;
+	private HashMap<Rol, Integer> mapRoles; // Es entero por la cantidad de roles repetibles
+	private List<Artista> artistasAsignados;
+
+	public Cancion(String nombreCancion, double duracion, Map<Rol, Integer> rolesRequeridos) {
+		this.nombreCancion = nombreCancion;
+		this.duracion = duracion;
+		this.mapRoles = new HashMap<>(rolesRequeridos);
+		this.artistasAsignados = new ArrayList<>();
+	}
+
+	public String getNombreCancion() {
+		return this.nombreCancion;
+	}
+
+	public double getDuracion() {
+		return this.duracion;
+	}
+
+	public List<Artista> getListArtAsignados() {
+		return this.artistasAsignados;
+	}
+
+	public HashMap<Rol, Integer> getMapRoles() {
+		return this.mapRoles;
+	}
+
+	/**
+	 * Verifica si la canción está completa (todos los roles cubiertos)
+	 */
+	public boolean puestosCubiertos() {
+		for (Entry<Rol, Integer> entry : mapRoles.entrySet()) {
+			if (entry.getValue() != 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Ocupa un rol con un artista
+	 * Busca un rol compatible entre los que tiene el artista y los que necesita la canción
+	 * @return true si se asignó exitosamente, false si no fue posible
+	 */
+	public boolean ocuparRol(Artista otro) throws Exception {
+		if (this.artistasAsignados.contains(otro)) {
+			throw new Exception("El artista ya está asignado en un rol para la canción");
+		}
+
+		// Buscar en roles históricos
+		for (Rol rolArtista : otro.getRolesHistoricos()) {
+			if (mapRoles.containsKey(rolArtista) && mapRoles.get(rolArtista) > 0) {
+				// Le bajo 1 a la cantidad de artista de ese rol
+				mapRoles.put(rolArtista, mapRoles.get(rolArtista) - 1);
+				// Agregamos al artista nuevo
+				artistasAsignados.add(otro);
+				return true;
+			}
+		}
+
+		// Si no encontró en históricos, buscar en roles entrenados
+		for (Rol rolEntrenado : otro.getRolesEntenados()) {
+			if (mapRoles.containsKey(rolEntrenado) && mapRoles.get(rolEntrenado) > 0) {
+				// Decrementar el rol disponible
+				mapRoles.put(rolEntrenado, mapRoles.get(rolEntrenado) - 1);
+				// Agregar el artista
+				artistasAsignados.add(otro);
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * Desocupa un rol (quita la asignación de un artista)
+	 */
+	public void descuparRol(Artista artista) throws Exception {
+		if (!artistasAsignados.contains(artista)) {
+			throw new Exception("No puedo desocupar un artista que no existe");
+		}
+
+		// Encontrar un rol que ocupaba
+		Rol rolOcupado = null;
+		for (Rol rol : artista.getRolesHistoricos()) {
+			if (mapRoles.containsKey(rol)) {
+				rolOcupado = rol;
+			}
+		}
+
+		if (rolOcupado == null) {
+			for (Rol rol : artista.getRolesEntenados()) {
+				if (mapRoles.containsKey(rol)) {
+					rolOcupado = rol;
+				}
+			}
+		}
+
+		// Remover artista
+		artistasAsignados.remove(artista);
+
+		// Incrementar el rol disponible
+		if (rolOcupado != null && mapRoles.containsKey(rolOcupado)) {
+			mapRoles.put(rolOcupado, mapRoles.get(rolOcupado) + 1);
+		}
+	}
+
+	/**
+	 * Retorna los roles faltantes
+	 */
+	public List<Rol> consultarRolesFaltantes() {
+		List<Rol> rolesFaltantes = new ArrayList<>();
+		for (Entry<Rol, Integer> entry : mapRoles.entrySet()) {
+			for (int i = 0; i < entry.getValue(); i++) {
+				rolesFaltantes.add(entry.getKey());
+			}
+		}
+		return rolesFaltantes;
+	}
+
+	@Override
+	public String toString() {
+		return "Cancion{" + "nombre='" + nombreCancion + '\'' + ", duracion=" + duracion + ", artistas asignados="
+				+ artistasAsignados.size() + ", roles faltantes=" + consultarRolesFaltantes().size() + '}';
+	}
+}
